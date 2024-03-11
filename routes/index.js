f94418c3a55e7688fb1d994eaac7cfb5f987d758
Josue Flores <josue.flores20@gmail.com>
2024-03-11 17:24:43 -0500

    remove unused variables in index

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { BAD_REQUEST, NOT_FOUND, DEFAULT } = require("../utils/errors");
+const { NOT_FOUND } = require("../utils/errors");
 
 const userRouter = require("./users");
 const clothingRouter = require("./clothingItems");
